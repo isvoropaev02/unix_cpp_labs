d0c874c08cf1b9bc2c5a8a76eae39fd01bc70d4a
isvoropaev02 <is.voropaev@gmail.com>
2025-04-18 19:08:43 +0300

    proc works

--- lab4/src/proc_parallel.cpp
+++ lab4/src/proc_parallel.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define READ_END	0
+#define READ_END    0
 #define WRITE_END	1
 
 inline double formula1(const double x) { return x * x - x * x + x * 4 - x * 5 + x + x; }
@@ -19,75 +19,53 @@ int main()
     std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<> dis(0.0, 1.0);
     double x = dis(gen);
-    size_t num_loops{10000};
+    size_t num_loops{100000};
 
     clock_t time_start = clock();
 
-    int fd1[2], fd2[2];
-    int result1{pipe(fd1)}, result2{pipe(fd2)};
-    if (result1 == -1 || result2 == -1) {
-        std::cout << "[ERROR] Failed to create pipes.\n";
-        exit(1);
+    int fd1[2];
+    int result1{pipe(fd1)};
+    if (result1 == -1) {
+        std::cout << "[ERROR] Failed to create pipe-channel.\n";
+        return 1;
     }
     pid_t pid1 = fork();
     if (pid1 == -1) {
         std::cout << "[ERROR] Failed in creating proc. with fork().\n";
-        // return 1;
-        exit(1);
-    } else if (pid1 == 0) {
-        // First child process
+        return 1;
+    }
+    else if (pid1 == 0)
+    {   // child process
+        close(fd1[READ_END]); // Close read end of pipe 1
+        std::vector<double> val1(num_loops);
+        for (int i = 0; i < num_loops; i++) {
+            val1[i] = formula1(x);
+            // std::cout << "Child Process (PID: " << getpid() << ") val1: " << val1[i] <<"\n";
+        }
+        write(fd1[WRITE_END], val1.data(), val1.size()*sizeof(double)); // Write result to pipe 1
+        close(fd1[WRITE_END]); // Close write end of pipe 1
+        exit(0);
+    }
+    else
+    {   // parrent process
+        close(fd1[WRITE_END]);
+        double val3;
+        std::vector<double> val1(num_loops);
+        std::vector<double> val2(num_loops);
+        for (int i = 0; i < num_loops; i++) {
+            val2[i] = formula2(x);
+            // std::cout << "Child Process (PID: " << getpid() << ") val2:" << val2[i] <<"\n";
+        }
         
-        close(fd1[0]); // Close read end of pipe 1
-        close(fd2[0]); // Close read end of pipe 2
-        close(fd2[1]); // Close write end of pipe 2
-        float calc_1[num_loops];
+        read(fd1[READ_END], val1.data(), val1.size()*sizeof(double)); // Read result from pipe
+        close(fd1[READ_END]);
         for (int i = 0; i < num_loops; i++) {
-            calc_1[i] = formula1(x);
-            // std::cout << "p1\n";
+            val3 = val1[i] + val2[i] - val1[i];
+            // std::cout << "Child Process (PID: " << getpid() << ") val3: " << val3 <<"\n";
         }
-        write(fd1[1], calc_1, sizeof(calc_1)); // Write result to pipe 1
-        close(fd1[1]); // Close write end of pipe 1
-        // return 0;
-        exit(0);
+        // Wait for child processes to exit
+        waitpid(pid1, nullptr, 0);
     }
-    pid_t pid2 = fork();
-    if (pid2 == -1) {
-        std::cout << "[ERROR] Failed in creating proc. with fork().\n";
-        // return 1;
-        exit(1);
-    } else if (pid2 == 0) {
-        // Second child process
-        close(fd2[0]); // Close read end of pipe 2
-        close(fd1[0]); // Close read end of pipe 1
-        close(fd2[1]); // Close write end of pipe 2
-        float calc_2[num_loops];
-        for (int i = 0; i < num_loops; i++) {
-            calc_2[i] = formula2(x);
-            // std::cout << "p2\n";
-        }
-        write(fd2[1], calc_2, sizeof(calc_2)); // Write result to pipe 2
-        close(fd2[1]); // Close write end of pipe 2
-        // return 0;
-        exit(0);
-    }
-
-    // Wait for child processes to exit
-    int status;
-    waitpid(pid1, &status, 0);
-    waitpid(pid2, &status, 0);
-    // Parent process
-    close(fd1[0]); // Close read end of pipe 1
-    close(fd1[1]); // Close write end of pipe 1
-    close(fd2[1]); // Close write end of pipe 2
-    float  calc_3;
-    float calc_2[num_loops];
-    read(fd2[0], calc_2, sizeof(calc_2)); // Read result from pipe 2
-    float calc_1[num_loops];
-    read(fd1[0], calc_1, sizeof(calc_1)); // Read result from pipe 1
-    for (int i = 0; i < num_loops; i++) {
-        calc_3 = calc_1[i] + calc_2[i] - calc_1[i];
-    }
-
     clock_t time_end = clock();
     double time = (double(time_end - time_start) / CLOCKS_PER_SEC);
     std::cout << time*1e6 << " us spent on " << num_loops << " loops" << "\n";
