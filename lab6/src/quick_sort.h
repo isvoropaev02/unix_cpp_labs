b497c322b672aab17273d8f943a79210ca91b15b
isvoropaev02 <is.voropaev@gmail.com>
2025-07-02 19:44:29 +0300

    quicksort works

---
+++ lab6/src/quick_sort.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+#include <chrono>
+
+template <typename T>
+int partition(std::vector<T>& arr, size_t low, size_t high) {
+    T pivot = arr[high-1];
+    size_t i = low;
+
+    for (size_t j = low; j < high-1; ++j) {
+        if (arr[j] < pivot) {
+            std::swap(arr[i], arr[j]);
+            ++i;
+        }
+    }
+    std::swap(arr[i], arr[high-1]);
+    return i;
+}
+
+template <typename T>
+void quick_sort_core(std::vector<T>& in_array, size_t low, size_t high) {
+    if (low < high) {
+        size_t pivot_idx = partition(in_array, low, high);
+        quick_sort_core(in_array, low, pivot_idx);
+        quick_sort_core(in_array, pivot_idx + 1, high);
+    }
+}
+
+template <typename T>
+int64_t quick_sort(std::vector<T>& in_array) {
+    auto task_start = std::chrono::high_resolution_clock::now();
+    quick_sort_core(in_array, 0, in_array.size());
+    auto task_end = std::chrono::high_resolution_clock::now();
+    return (std::chrono::duration_cast<std::chrono::microseconds>(task_end - task_start)).count();
+}
