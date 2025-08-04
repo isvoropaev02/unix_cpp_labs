6b3ea326c93c04c54aa5f55da897a7db96a6a0e5
isvoropaev02 <is.voropaev@gmail.com>
2025-08-04 14:58:47 +0300

    more complicated city

--- lab8/main.py
+++ lab8/main.py
@@ -2,13 +2,20 @@ from road_elements import *
 from vehicle_route_generator import VehicleGenerator
 
 '''Simulation configuration'''
-CROSS_ROADS = {0: CrossRoad(coords=(0, 0), road_id_vs_ports=[0, -1, -1, -1], tr_light_duration_s=20.),
-               1: CrossRoad(coords=(300., 0.), road_id_vs_ports=[0, 1, -1, -1], tr_light_duration_s=15),
-               2: CrossRoad(coords=(300., 150.), road_id_vs_ports=[1, -1, -1, -1], tr_light_duration_s=22)}
+CROSS_ROADS = {0: CrossRoad(coords=(0, 0), road_id_vs_ports=[0, 1, -1, -1], tr_light_duration_s=20.),
+               1: CrossRoad(coords=(0., 300.), road_id_vs_ports=[0, 2, -1, -1], tr_light_duration_s=15),
+               2: CrossRoad(coords=(400., 300.), road_id_vs_ports=[1, 2, 3, 4], tr_light_duration_s=22),
+               3: CrossRoad(coords=(450., 500.), road_id_vs_ports=[3, 5, -1, -1], tr_light_duration_s=18),
+               4: CrossRoad(coords=(600., 300.), road_id_vs_ports=[5, 4, -1, -1], tr_light_duration_s=27)}
 ROADS = {0: Road((0, 0), (1, 0)),
-         1: Road((1, 1), (2, 0))}
+         1: Road((0, 1), (2, 0)),
+         2: Road((1, 1), (2, 1)),
+         3: Road((2, 2), (3, 0)),
+         4: Road((2, 3), (4, 1)),
+         5: Road((3, 1), (4, 0))}
 CARS = dict()
 CAR_SPAWN_RATE = 20     # every x seconds a new car appears
+CAR_ROUTE_LENGHT = 4    # num of roads in route
 SIM_DURATION = 120      # sec
 DELTA_T = 1             # sec
 
@@ -16,7 +23,7 @@ DELTA_T = 1             # sec
 global_time_s = 0
 next_car_id = 0
 
-route_gen = VehicleGenerator(2)
+route_gen = VehicleGenerator(CAR_ROUTE_LENGHT)
 
 while global_time_s < SIM_DURATION:
     print(f"GLOBAL TIME: {global_time_s} sec")
